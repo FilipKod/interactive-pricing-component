5c279fcf5a2a3b42d1b29707f410b1a8d4998640
Filip Madunicky <yelidric@gmail.com>
2023-07-26 20:16:23 +0200

    update interactive elements

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 "use strict";
-const rangeValues = ["10K", "50K", "100K", "500K", "1M"];
-const inputRange = document.getElementById("inputRange");
-const outputRange = document.getElementById("outputRange");
-if (inputRange && outputRange) {
-    const changeRangeSliderSize = () => {
-        const maxVal = +inputRange.max;
-        const actualVal = +inputRange.value;
-        const newRangeSize = (actualVal / maxVal) * 100;
-        inputRange.style.backgroundSize = `${newRangeSize}% 100%`;
-    };
-    const inputChangeHandler = () => {
-        const newValue = +inputRange.value;
-        outputRange.innerHTML = rangeValues[newValue];
-        changeRangeSliderSize();
-    };
-    inputRange.addEventListener("input", inputChangeHandler);
-    inputRange.value = Math.floor(+inputRange.max / 2).toString();
+const rawData = [
+    { views: "10K", price: 8 },
+    { views: "50K", price: 12 },
+    { views: "100K", price: 16 },
+    { views: "500K", price: 24 },
+    { views: "1M", price: 36 },
+];
+const discountInPercent = 25;
+const updatedData = rawData.map((obj) => (Object.assign(Object.assign({}, obj), { pricePerYear: obj.price - obj.price * (discountInPercent / 100) })));
+const viewsRangeInputElement = document.getElementById("viewsRangeInput");
+const pageViewsElement = document.getElementById("pageViews");
+const displayPriceElement = document.getElementById("displayPrice");
+const yearlySubCheckElement = document.getElementById("yearlySubCheck");
+const discountPercentSpan = document.getElementById("discountPercent");
+function displayPrice(rangeValue) {
+    const checked = yearlySubCheckElement === null || yearlySubCheckElement === void 0 ? void 0 : yearlySubCheckElement.checked;
+    const price = checked
+        ? updatedData[rangeValue].pricePerYear
+        : updatedData[rangeValue].price;
+    if (displayPriceElement) {
+        displayPriceElement.innerHTML = "$" + price.toFixed(2);
+    }
 }
+yearlySubCheckElement === null || yearlySubCheckElement === void 0 ? void 0 : yearlySubCheckElement.addEventListener("change", function (event) {
+    const rangeValue = viewsRangeInputElement && +viewsRangeInputElement.value;
+    if (rangeValue != null) {
+        displayPrice(rangeValue);
+    }
+});
+viewsRangeInputElement === null || viewsRangeInputElement === void 0 ? void 0 : viewsRangeInputElement.addEventListener("input", function (event) {
+    const target = event.target;
+    if (target && pageViewsElement && displayPriceElement) {
+        const value = +target.value;
+        const max = +target.max;
+        const bgSize = (value / max) * 100;
+        pageViewsElement.innerHTML = updatedData[value].views;
+        target.style.backgroundSize = `${bgSize}% 100%`;
+        displayPrice(value);
+    }
+});
+// setup default values
+if (viewsRangeInputElement)
+    viewsRangeInputElement.value = Math.floor(+viewsRangeInputElement.max / 2).toString();
+if (yearlySubCheckElement)
+    yearlySubCheckElement.checked = false;
+if (discountPercentSpan)
+    discountPercentSpan.innerHTML = `-${discountInPercent}%`;
--- main.ts
+++ main.ts
@@ -1,30 +1,82 @@
-const rangeValues: string[] = ["10K", "50K", "100K", "500K", "1M"];
+interface IData {
+  views: string;
+  price: number;
+}
 
-const inputRange = document.getElementById(
-  "inputRange"
+const rawData: IData[] = [
+  { views: "10K", price: 8 },
+  { views: "50K", price: 12 },
+  { views: "100K", price: 16 },
+  { views: "500K", price: 24 },
+  { views: "1M", price: 36 },
+];
+
+const discountInPercent = 25;
+
+const updatedData = rawData.map((obj) => ({
+  ...obj,
+  pricePerYear: obj.price - obj.price * (discountInPercent / 100),
+}));
+
+const viewsRangeInputElement = document.getElementById(
+  "viewsRangeInput"
 ) as HTMLInputElement | null;
-const outputRange = document.getElementById(
-  "outputRange"
+const pageViewsElement = document.getElementById(
+  "pageViews"
+) as HTMLSpanElement | null;
+const displayPriceElement = document.getElementById(
+  "displayPrice"
+) as HTMLSpanElement | null;
+const yearlySubCheckElement = document.getElementById(
+  "yearlySubCheck"
+) as HTMLInputElement | null;
+const discountPercentSpan = document.getElementById(
+  "discountPercent"
 ) as HTMLSpanElement | null;
 
-if (inputRange && outputRange) {
-  const changeRangeSliderSize = () => {
-    const maxVal = +inputRange.max;
-    const actualVal = +inputRange.value;
+function displayPrice(rangeValue: number) {
+  const checked = yearlySubCheckElement?.checked;
 
-    const newRangeSize = (actualVal / maxVal) * 100;
+  const price = checked
+    ? updatedData[rangeValue].pricePerYear
+    : updatedData[rangeValue].price;
 
-    inputRange.style.backgroundSize = `${newRangeSize}% 100%`;
-  };
-
-  const inputChangeHandler = () => {
-    const newValue = +inputRange.value;
-    outputRange.innerHTML = rangeValues[newValue];
-
-    changeRangeSliderSize();
-  };
-
-  inputRange.addEventListener("input", inputChangeHandler);
-
-  inputRange.value = Math.floor(+inputRange.max / 2).toString();
+  if (displayPriceElement) {
+    displayPriceElement.innerHTML = "$" + price.toFixed(2);
+  }
 }
+
+yearlySubCheckElement?.addEventListener("change", function (event) {
+  const rangeValue = viewsRangeInputElement && +viewsRangeInputElement.value;
+
+  if (rangeValue != null) {
+    displayPrice(rangeValue);
+  }
+});
+
+viewsRangeInputElement?.addEventListener("input", function (event) {
+  const target = event.target as HTMLInputElement | null;
+
+  if (target && pageViewsElement && displayPriceElement) {
+    const value = +target.value;
+    const max = +target.max;
+
+    const bgSize = (value / max) * 100;
+
+    pageViewsElement.innerHTML = updatedData[value].views;
+    target.style.backgroundSize = `${bgSize}% 100%`;
+
+    displayPrice(value);
+  }
+});
+
+// setup default values
+if (viewsRangeInputElement)
+  viewsRangeInputElement.value = Math.floor(
+    +viewsRangeInputElement.max / 2
+  ).toString();
+
+if (yearlySubCheckElement) yearlySubCheckElement.checked = false;
+
+if (discountPercentSpan)
+  discountPercentSpan.innerHTML = `-${discountInPercent}%`;
